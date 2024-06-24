96e637411a05a30ce9db368e1eb8e87c55c0c664
Caua Fernndo <cauafernadodesouza076@gmail.com>
2024-06-24 19:03:22 -0300

    Criando uma cqlculadora

---
+++ assets/js/index.js
@@ -0,0 +1,19 @@
+function insert(num)  {
+   const numer = document.getElementById('result').innerHTML;
+   document.getElementById('result').innerHTML = numer + num;
+    
+}
+function clean() {
+    document.getElementById('result').innerHTML ="";
+}
+function back() {
+    const result = document.getElementById('result').innerHTML;
+    document.getElementById('result').innerHTML = result.substring(0, result.length -1);
+}
+document.getElementById('btnr').addEventListener('click', ()=> {
+    const result = document.getElementById('result').innerHTML.replace('x', '*');
+    if (result){
+        document.getElementById('result').innerHTML = eval(result);
+    }
+    
+})
